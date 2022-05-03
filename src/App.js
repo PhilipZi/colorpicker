1582e34a54048f23cf912517a65776b621d7323b
Philip Zimmermann <philip.zimmermann@freenet.de>
2022-05-03 15:44:27 +0200

    Add Card and colors

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  const colors = ["#ccc", "#4c6ef5", "#82c91e", "#12b886", "#82c91e", "#ccc"];
+
   return (
     <div className="App">
       <h1>Quiz App</h1>
-      <Card></Card>
+      {colors.map((color) => {
+        return <Card key={color} color={color} />;
+      })}
     </div>
   );
 }
